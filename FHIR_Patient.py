3681f049a58fa822617903cc725e9c5f4c8b1e31
MusMuss1 <suppport.ydownload@gmail.com>
2020-06-20 20:22:57 +0200

    *Changes *add loop to get all names and telecom

--- FHIR_Patient.py
+++ FHIR_Patient.py
@@ -1,4 +1,9 @@
 from fhirclient import client
+
+i=0
+paddress = ""
+ptelecom = ""
+
 settings ={
     'app_id': 'my_web_app',
     'api_base': 'https://api.logicahealth.org/MST/open/'
@@ -8,13 +13,17 @@ smart = client.FHIRClient(settings=settings)
 import fhirclient.models.patient as p
 
 patient = p.Patient.read('17852', smart.server)
-
+pnamee = smart.human_name(patient.name[0])
 
 pbirthday = patient.birthDate.isostring
-pnamee = smart.human_name(patient.name[0])
-pname = patient.name[0].family
-pvname = patient.name[0].given[0]
-pvname1 = patient.name[0].given[1]
+
+pname = patient.name[0].family + ","
+
+while i < len(patient.name[0].given):
+    pname += " "
+    pname += (patient.name[0].given[i])
+    i+=1
+
 pgender = patient.gender
 
 pcity = patient.address[0].city
@@ -25,16 +34,14 @@ pcountry = patient.address[0].country
 
 paddress = phome + ", " + ppostal + " " + pcity + ", " + pstate + ", " + pcountry
 
-psys = patient.telecom[0].system
-pvalue = patient.telecom[0].value
-puse = patient.telecom[0].use
+i=0
+while i < len(patient.telecom):
+    ptelecom += patient.telecom[i].system
+    ptelecom += ": "
+    ptelecom += patient.telecom[i].value
+    ptelecom += "\n"
+    i+=1
 
-pphone = psys + ": " + puse + ", " + pvalue
+print("Name, Vorname: " + pname + " "  + "\n" + "Geburtsdatum: " + pbirthday + "\n" + "Geschlecht: " + pgender + "\n" + "Adresse: " + paddress + "\n" + ptelecom)
 
-psys2 = patient.telecom[1].system
-pvalue2 = patient.telecom[1].value
-
-pmaail = psys2 + ": " + pvalue2
-
-print("Name, Vorname: " + pname + ", " + pvname + " " +pvname1 + "\n" + "Geburtsdatum: " + pbirthday + "\n" + "Geschlecht: " + pgender + "\n" + "Adresse: " + paddress + "\n" + pphone + "\n" + pmaail)
-
+print(pnamee)
