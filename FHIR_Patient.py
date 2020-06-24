3170e5e14879170acf4afc6177767184721f251b
MusMuss1 <suppport.ydownload@gmail.com>
2020-06-24 20:02:14 +0200

    *Changes *add input

--- FHIR_Patient.py
+++ FHIR_Patient.py
@@ -10,18 +10,26 @@ settings ={
 }
 smart = client.FHIRClient(settings=settings)
 
+Input = input("Insert ID !\n")
+
 import fhirclient.models.patient as p
 import fhirclient.models.practitioner as pr
 import fhirclient.models.encounter as e
 import fhirclient.models.observation as o
 
-patient = p.Patient.read('17852', smart.server)
 practitioner = pr.Practitioner.read('16889', smart.server)
 encounter = e.Encounter.read('17903', smart.server)
-observation = o.Observation.read('17858', smart.server)
 
+#Observation
+observation = o.Observation.read(Input, smart.server)
+operfomer = observation.performer[0].reference
+
+print(operfomer + " found \n")
+operfomer = int("".join(filter(str.isdigit, operfomer)))
+
+#Patient
+patient = p.Patient.read(operfomer, smart.server)
 pbirthday = patient.birthDate.isostring
-
 pnamee = smart.human_name(patient.name[0])
 pname = patient.name[0].family + ","
 
