a36c5431ae3e71fb08adea6752457cffa82ed085
MusMuss1 <suppport.ydownload@gmail.com>
2020-07-12 15:29:44 +0200

    *Changes *fixed input crash with try catch

--- FHIR_Patient.py
+++ FHIR_Patient.py
@@ -97,199 +97,221 @@ head = "Liste aller "+Input+" Patienten"
 kuerzel = Input
 askhtml(kuerzel,head)
 
-Input = input("insert Patient ID !\n")
-patID = str(Input)
-
-#Patient
-patient = p.Patient.read(patID, smart.server)
-pbirthday = patient.birthDate.isostring
-pnamee = smart.human_name(patient.name[0])
-pname = patient.name[0].family + ","
-
-while i < len(patient.name[0].given):
-    pname += " "
-    pname += (patient.name[0].given[i])
-    i+=1
-
-pgender = patient.gender
-
-pcity = patient.address[0].city
-ppostal = patient.address[0].postalCode
-phome = patient.address[0].line[0]
-pstate = patient.address[0].state
-pcountry = patient.address[0].country
-
-paddress = phome + ", " + ppostal + " " + pcity + ", " + pstate + ", " + pcountry
-
-i=0
-while i < len(patient.telecom):
-    ptelecom += patient.telecom[i].system
-    ptelecom += ": "
-    ptelecom += patient.telecom[i].value
-    ptelecom += "\n"
-    i+=1
-
-print("Patient "+patID+" gefunden.\n")
-print("Name, Vorname: " + pname + " "  + "\n" + "Geburtsdatum: " + pbirthday + "\n" + "Geschlecht: " + pgender + "\n" + "Adresse: " + paddress + "\n" + ptelecom)
-
-#show all observations
-oID =[]
-opID =[]
-oCode =[]
-oCodeValue =[]
-oDate =[]
-oValue =[]
-oName =[]
-
-search = o.Observation.where(struct={'performer': patID})
-observations = search.perform_resources(smart.server)
-for obser in observations:
-    oID.append(obser.id)
-    opID.append(obser.performer[0].reference)
-    oDate.append(obser.effectiveDateTime.isostring)
-    oCodeValue.append(obser.code.coding[0].display)
-    oCode.append(obser.code.coding[0].code)
+valid=False
+while not valid:
     try:
-        oValue.append(str(obser.valueQuantity.value) + " " + obser.valueQuantity.unit)
+        Input = input("insert Patient ID !\n")
+        patID = str(Input)
+
+        # Patient
+        patient = p.Patient.read(patID, smart.server)
+        pbirthday = patient.birthDate.isostring
+        pnamee = smart.human_name(patient.name[0])
+        pname = patient.name[0].family + ","
+
+        while i < len(patient.name[0].given):
+            pname += " "
+            pname += (patient.name[0].given[i])
+            i += 1
+
+        pgender = patient.gender
+
+        pcity = patient.address[0].city
+        ppostal = patient.address[0].postalCode
+        phome = patient.address[0].line[0]
+        pstate = patient.address[0].state
+        pcountry = patient.address[0].country
+
+        paddress = phome + ", " + ppostal + " " + pcity + ", " + pstate + ", " + pcountry
+
+        i = 0
+        while i < len(patient.telecom):
+            ptelecom += patient.telecom[i].system
+            ptelecom += ": "
+            ptelecom += patient.telecom[i].value
+            ptelecom += "\n"
+            i += 1
+
+        print("Patient " + patID + " gefunden.\n")
+        print(
+            "Name, Vorname: " + pname + " " + "\n" + "Geburtsdatum: " + pbirthday + "\n" + "Geschlecht: " + pgender + "\n" + "Adresse: " + paddress + "\n" + ptelecom)
+
+        # show all observations
+        oID = []
+        opID = []
+        oCode = []
+        oCodeValue = []
+        oDate = []
+        oValue = []
+        oName = []
+
+        search = o.Observation.where(struct={'performer': patID})
+        observations = search.perform_resources(smart.server)
+        for obser in observations:
+            oID.append(obser.id)
+            opID.append(obser.performer[0].reference)
+            oDate.append(obser.effectiveDateTime.isostring)
+            oCodeValue.append(obser.code.coding[0].display)
+            oCode.append(obser.code.coding[0].code)
+            try:
+                oValue.append(str(obser.valueQuantity.value) + " " + obser.valueQuantity.unit)
+            except:
+                oValue.append(obser.valueString)
+            oName.append(pname)
+
+        dfObserAll = DataFrame({"Observation ID": oID,
+                                "Performer ID": opID,
+                                "Name": oName,
+                                "Date": oDate,
+                                "Code": oCode,
+                                "Code Value": oCodeValue,
+                                "Messungen": oValue})
+
+        print("Verlaufsübersicht persönlicher Messungen.")
+        print(dfObserAll)
+        html = dfObserAll.to_html()
+        head = "Liste aller Observationen von " + patID
+        kuerzel = "Observation_" + patID
+        askhtml(kuerzel, head)
+        valid = True
     except:
-        oValue.append(obser.valueString)
-    oName.append(pname)
+        print(FileNotFoundError)
 
-dfObserAll = DataFrame({ "Observation ID": oID,
-                        "Performer ID": opID,
-                        "Name": oName,
-                        "Date": oDate,
-                        "Code": oCode,
-                        "Code Value": oCodeValue,
-                        "Messungen": oValue})
+valid = False
+while not valid:
+    try:
+        # show encounters
+        eID = []
+        eStart = []
+        eEnd = []
+        eValue = []
+        eDoc = []
+        eDocID = []
+        eSub = []
 
-print("Verlaufsübersicht persönlicher Messungen.")
-print(dfObserAll)
-html = dfObserAll.to_html()
-head = "Liste aller Observationen von "+ patID
-kuerzel = "Observation_"+patID
-askhtml(kuerzel,head)
+        search = e.Encounter.where(struct={'subject': patID})
+        encounters = search.perform_resources(smart.server)
+        for enc in encounters:
+            eID.append(enc.id)
+            eStart.append(enc.period.start.isostring)
+            eEnd.append(enc.period.end.isostring)
+            eDoc.append(enc.participant[0].individual.display)
+            eDocID.append(enc.participant[0].individual.reference)
+            eSub.append(pname)
+            # pat = int("".join(filter(str.isdigit, enc.subject.reference)))
+            # patient = p.Patient.read(pat, smart.server)
+            # eSub.append(str(smart.human_name(patient.name[0])))
 
-#show encounters
-eID =[]
-eStart =[]
-eEnd =[]
-eValue =[]
-eDoc =[]
-eDocID =[]
-eSub =[]
+        dfEnc = DataFrame({"Encounter ID": eID,
+                           "Start": eStart,
+                           "End": eEnd,
+                           "Doc": eDoc,
+                           "Doc ID": eDocID,
+                           "Patient": eSub})
 
-search = e.Encounter.where(struct={'subject': patID})
-encounters = search.perform_resources(smart.server)
-for enc in encounters:
-    eID.append(enc.id)
-    eStart.append(enc.period.start.isostring)
-    eEnd.append(enc.period.end.isostring)
-    eDoc.append(enc.participant[0].individual.display)
-    eDocID.append(enc.participant[0].individual.reference)
-    eSub.append(pname)
-    #pat = int("".join(filter(str.isdigit, enc.subject.reference)))
-    #patient = p.Patient.read(pat, smart.server)
-    #eSub.append(str(smart.human_name(patient.name[0])))
+        print(dfEnc)
+        print("\n")
+        html = dfEnc.to_html()
+        head = "Liste aller Encounter von " + patID
+        kuerzel = "Encounter_" + patID
+        askhtml(kuerzel, head)
+        valid = True
+    except:
+        print(FileNotFoundError + "\nNo encounters found")
 
+observations.clear()
+while not observations:
+    try:
+        # show encouterobservation
+        oID = []
+        opID = []
+        oCode = []
+        oCodeValue = []
+        oDate = []
+        oValue = []
+        oName = []
 
-dfEnc = DataFrame({   "Encounter ID": eID,
-                      "Start": eStart,
-                      "End": eEnd,
-                      "Doc": eDoc,
-                      "Doc ID":eDocID,
-                      "Patient": eSub})
+        Input = input("Enter Practitioner ID !\n")
 
-print(dfEnc)
-print("\n")
-html = dfEnc.to_html()
-head = "Liste aller Encounter von "+ patID
-kuerzel = "Encounter_"+patID
-askhtml(kuerzel,head)
+        search = o.Observation.where(struct={'performer': Input, 'subject': patID, 'status': 'final'})
+        observations = search.perform_resources(smart.server)
+        for obser in observations:
+            oID.append(obser.id)
+            opID.append(obser.performer[0].reference)
+            oDate.append(obser.effectiveDateTime.isostring)
+            oCodeValue.append(obser.code.coding[0].display)
+            oCode.append(obser.code.coding[0].code)
+            try:  # Wert+Einheit
+                oValue.append(str(obser.valueQuantity.value) + " " + obser.valueQuantity.unit)
+            except:  # Versuche Blutdruck
+                try:
+                    oValue.append(
+                        "(" + obser.component[0].code.coding[0].display + "/" + obser.component[1].code.coding[
+                            0].display + ") " + str(obser.component[0].valueQuantity.value) + "/" + str(
+                            obser.component[1].valueQuantity.value) + " " + obser.component[0].valueQuantity.unit)
+                except:  # Versuche Schlafverhalten
+                    oValue.append(obser.valueString)
+            oName.append(pname)
 
-#show encouterobservation
-oID = []
-opID = []
-oCode = []
-oCodeValue = []
-oDate = []
-oValue = []
-oName = []
+        dfObser = DataFrame({"Observation ID": oID,
+                             "Performer ID": opID,
+                             "Patient": oName,
+                             "Date": oDate,
+                             "Code": oCode,
+                             "Code Value": oCodeValue,
+                             "Messungen": oValue})
 
-Input =input("Enter Practitioner ID !\n")
-
-search = o.Observation.where(struct={'performer': Input,'subject': patID, 'status': 'final'})
-observations = search.perform_resources(smart.server)
-for obser in observations:
-    oID.append(obser.id)
-    opID.append(obser.performer[0].reference)
-    oDate.append(obser.effectiveDateTime.isostring)
-    oCodeValue.append(obser.code.coding[0].display)
-    oCode.append(obser.code.coding[0].code)
-    try: #Wert+Einheit
-        oValue.append(str(obser.valueQuantity.value) + " " + obser.valueQuantity.unit)
-    except:#Versuche Blutdruck
-        try:
-            oValue.append("(" + obser.component[0].code.coding[0].display + "/" + obser.component[1].code.coding[
-                0].display + ") " + str(obser.component[0].valueQuantity.value) + "/" + str(
-                obser.component[1].valueQuantity.value) + " " + obser.component[0].valueQuantity.unit)
-        except:#Versuche Schlafverhalten
-            oValue.append(obser.valueString)
-    oName.append(pname)
-
-dfObser = DataFrame({"Observation ID": oID,
-                     "Performer ID": opID,
-                     "Patient": oName,
-                     "Date": oDate,
-                     "Code": oCode,
-                     "Code Value": oCodeValue,
-                     "Messungen": oValue})
-
-print(dfObser)
-print("\n")
-html = dfObser.to_html()
-head = "Liste aller Encounter Observationen von "+ patID
-kuerzel = "Encounter_"+patID
-askhtml(kuerzel,head)
-
+        print(dfObser)
+        print("\n")
+        html = dfObser.to_html()
+        head = "Liste aller Encounter Observationen von " + patID
+        kuerzel = "Encounter_" + patID
+        if observations:
+            askhtml(kuerzel, head)
+    except:
+        print(FileNotFoundError)
 
 
 # show other observations
 def observa(Input):
-    oID = []
-    opID = []
-    oCode = []
-    oCodeValue = []
-    oDate = []
-    oValue = []
-    oName = []
+    try:
+        oID = []
+        opID = []
+        oCode = []
+        oCodeValue = []
+        oDate = []
+        oValue = []
+        oName = []
 
-    search = o.Observation.where(struct={'performer': patID, 'code': str(Input)})
-    observations = search.perform_resources(smart.server)
-    for obser in observations:
-        oID.append(obser.id)
-        opID.append(obser.performer[0].reference)
-        oDate.append(obser.effectiveDateTime.isostring)
-        oCodeValue.append(obser.code.coding[0].display)
-        oCode.append(obser.code.coding[0].code)
-        try:
-            oValue.append(str(obser.valueQuantity.value) + " " + obser.valueQuantity.unit)
-        except:
-            oValue.append(obser.valueString)
-        oName.append(pname)
+        search = o.Observation.where(struct={'performer': patID, 'code': str(Input)})
+        observations = search.perform_resources(smart.server)
+        for obser in observations:
+            oID.append(obser.id)
+            opID.append(obser.performer[0].reference)
+            oDate.append(obser.effectiveDateTime.isostring)
+            oCodeValue.append(obser.code.coding[0].display)
+            oCode.append(obser.code.coding[0].code)
+            try:
+                oValue.append(str(obser.valueQuantity.value) + " " + obser.valueQuantity.unit)
+            except:
+                oValue.append(obser.valueString)
+            oName.append(pname)
 
-    dfObser = DataFrame({   "Observation ID": oID,
-                            "Performer ID": opID,
-                            "Name": oName,
-                            "Date": oDate,
-                            "Code": oCode,
-                            "Code Value": oCodeValue,
-                            "Messungen": oValue})
+        dfObser = DataFrame({"Observation ID": oID,
+                             "Performer ID": opID,
+                             "Name": oName,
+                             "Date": oDate,
+                             "Code": oCode,
+                             "Code Value": oCodeValue,
+                             "Messungen": oValue})
 
-    kuerzel = "Observation_" + Input + "_" + patID
-    html = dfObser.to_html()
-    print(dfObser)
-    return html
+        kuerzel = "Observation_" + Input + "_" + patID
+        html = dfObser.to_html()
+        print(dfObser)
+        return html
+    except:
+        print(FileNotFoundError)
+
 
 while True:
     quest = input('Look for speci Observations ?\n')
@@ -310,14 +332,3 @@ if Fl == 'n':
 
 
 #17862
-
-"""    # Observation
-    observation = o.Observation.read(Input, smart.server)
-    operfomer = observation.performer[0].reference
-    code = observation.code.coding[0].code
-    codevalue = observation.code.coding[0].display
-
-    print(operfomer + " found \n")
-    operfomer = int("".join(filter(str.isdigit, operfomer)))
-
-    print("Other Observations Found")"""
